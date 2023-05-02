a3fe6d3f8fc5d8f8d82a2595bfc55e8bfc8603ef
drewjya <andre.wijaya004@binus.ac.id>
2023-05-02 23:31:21 +0700

    upd

--- lib/src/features/content/repository/content_repository_impl.dart
+++ lib/src/features/content/repository/content_repository_impl.dart
@@ -3,19 +3,21 @@ import 'package:beaverbasketball/src/features/content/model/youtube_model.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'content_repository.dart';
 
+//AIzaSyBYnhuaSpaPXeakbujqOqNIAuKsSNB49Vo
+//AIzaSyAUo0xmh0jiQffKxa8mK6fhxJBXX3uenJw
 class ContentRepositoryImpl extends ContentRepository {
   @override
   Future<List<YoutubeModel>> loadYoutubePost() async {
     final url =
-        "https://www.googleapis.com/youtube/v3/search?channelId=UCqQ32OBHgYtWcxCXjT-vRxQ&maxResults=6&key=AIzaSyAUo0xmh0jiQffKxa8mK6fhxJBXX3uenJw&order=date&part=snippet";
-        
+        "https://www.googleapis.com/youtube/v3/search?channelId=UCqQ32OBHgYtWcxCXjT-vRxQ&maxResults=6&key=AIzaSyBYnhuaSpaPXeakbujqOqNIAuKsSNB49Vo&order=date&part=snippet";
+
     final data = await HttpWrapper.get(
       url: url,
       fromJson: (p0) =>
           (p0["items"] as List).map((e) => YoutubeModel.fromMap(e)).toList(),
     );
-    
-    return data??[];
+
+    return data ?? [];
   }
 }
 
